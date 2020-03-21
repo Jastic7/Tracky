8e9705f318f3ca04fa1a94149ad7950c36ac32fd
Andrey Morozov <jastic7@yandex.ru>
2020-03-21 21:21:12 +0300

    Initial commit

---
+++ Tracky/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tracky
+//
+//  Created by Andrey Morozov on 21.03.2020.
+//  Copyright © 2020 Jastic7. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
